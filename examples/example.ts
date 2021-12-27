0b1792cdfc8afa89179f310ae0d6ef8443fe341d
Lou Cyx <git@lou.cx>
2021-12-27 02:54:20 -0800

    :tada: initial commit

---
+++ examples/example.ts
@@ -0,0 +1,15 @@
+/**
+ * Example JSDoc
+ * @class Foobar
+ */
+export class Foobar {
+	/**
+	 * Creates an instance of Foobar.
+	 *
+	 * @param {string} foo
+	 * @param {string} bar
+	 */
+	public constructor(foo: string, bar: string) {
+		console.log({ foo, bar });
+	}
+}
